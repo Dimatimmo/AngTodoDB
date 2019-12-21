4da6f677d4ae30026364dce0df0d15824e364a5e
Dimatimmo <dimatimmokh@gmail.com>
2019-12-21 20:52:15 +0200

    upload project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA5PMhmXWNa_-mnKLlGqWGF4-EApZ2Yd7s",
+    authDomain: "angtododb.firebaseapp.com",
+    databaseURL: "https://angtododb.firebaseio.com",
+    projectId: "angtododb",
+    storageBucket: "angtododb.appspot.com",
+    messagingSenderId: "529397500532",
+    appId: "1:529397500532:web:f4a3f1a597a96993be5f21",
+    measurementId: "G-K3YDRYMJY0"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA5PMhmXWNa_-mnKLlGqWGF4-EApZ2Yd7s",
+    authDomain: "angtododb.firebaseapp.com",
+    databaseURL: "https://angtododb.firebaseio.com",
+    projectId: "angtododb",
+    storageBucket: "angtododb.appspot.com",
+    messagingSenderId: "529397500532",
+    appId: "1:529397500532:web:f4a3f1a597a96993be5f21",
+    measurementId: "G-K3YDRYMJY0"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
